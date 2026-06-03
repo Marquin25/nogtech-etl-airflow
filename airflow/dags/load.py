76312c79f74e20c315366c23a44b4b3b4d78871f
Marquin25 <marcusantoniopina@gmail.com>
2026-06-03 08:59:50 -0300

    git commit -m "feat: carregar dados transformados no PostgreSQL"

--- airflow/dags/load.py
+++ airflow/dags/load.py
@@ -1,2 +1,19 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
 def load():
-    print("Carregando dados...")
+
+    df = pd.read_csv("/opt/airflow/data/resultado.csv")
+
+    engine = create_engine(
+        "postgresql+psycopg2://airflow:airflow@postgres:5432/nogtech"
+    )
+
+    df.to_sql(
+        "alunos_tratados",
+        engine,
+        if_exists="replace",
+        index=False
+    )
+
+    print("Dados carregados com sucesso!")
