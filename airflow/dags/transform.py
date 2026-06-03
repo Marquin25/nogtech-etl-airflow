aa19d3ff40e2662231f5080178597f74ef2648e0
theoleal <theojlleal@gmail.com>
2026-06-03 11:05:23 -0300

    feat: adiciona anonimizacao no cpf e tira nome dos alunos tratados

--- airflow/dags/transform.py
+++ airflow/dags/transform.py
@@ -2,11 +2,11 @@ import pandas as pd
 import re
 
 def formatar_cpf(cpf):
-
     cpf = re.sub(r"\D", "", str(cpf))
-
     return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
 
+def anonimizar_cpf(cpf):
+    return f"***.{cpf[4:7]}.{cpf[8:11]}-**"
 
 def transform():
 
@@ -20,15 +20,8 @@ def transform():
         "/opt/airflow/data/engajamento_alunos.json"
     )
 
-    transacoes["cpf_aluno"] = (
-        transacoes["cpf_aluno"]
-        .apply(formatar_cpf)
-    )
-
-    engajamento["cpf_aluno"] = (
-        engajamento["cpf_aluno"]
-        .apply(formatar_cpf)
-    )
+    transacoes["cpf_aluno"] = transacoes["cpf_aluno"].apply(formatar_cpf)
+    engajamento["cpf_aluno"] = engajamento["cpf_aluno"].apply(formatar_cpf)
 
     transacoes["mes_referencia"] = (
         pd.to_datetime(
@@ -43,6 +36,12 @@ def transform():
         on=["cpf_aluno", "mes_referencia"],
         how="left"
     )
+
+    # LGPD
+    resultado["cpf_aluno"] = resultado["cpf_aluno"].apply(anonimizar_cpf)
+
+    if "nome_aluno" in resultado.columns:
+        resultado.drop(columns=["nome_aluno"], inplace=True)
 
     resultado.to_csv(
         "/opt/airflow/data/resultado.csv",
